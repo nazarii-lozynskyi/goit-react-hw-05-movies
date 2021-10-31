0a038bf27674bf029fd699080940c3deeb3417d9
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-31 19:03:17 +0100

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,9 +1,17 @@
 import { useState, useEffect } from 'react';
-import { Link, useRouteMatch, useParams } from 'react-router-dom';
+import {
+  NavLink,
+  useRouteMatch,
+  useParams,
+  useHistory,
+  Route,
+} from 'react-router-dom';
 
 import PageHeading from '../../components/PageHeading';
+import Cast from '../../components/Cast';
+import Reviews from '../../components/Reviews';
 
-import { Card, Row, Tab, Col, ListGroup, Container } from 'react-bootstrap';
+import { Card, ListGroup, Container } from 'react-bootstrap';
 
 import styles from './MovieDetailsPage.module.css';
 
@@ -12,6 +20,7 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 
 function MovieDetailsPage() {
   const { url } = useRouteMatch();
+  const history = useHistory();
   const { movieId } = useParams();
 
   const URL = `${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
@@ -22,14 +31,15 @@ function MovieDetailsPage() {
       .then(response => response.json())
       .then(result => setMovie(result))
       .catch(err => console.warn(err));
-  }, []);
+  }, [URL]);
   return (
     <section className={styles.Section}>
       <Container>
-        <PageHeading text="Tr" />
+        <PageHeading text={movie.title} />
 
         <Card className="bg-dark text-white">
           <Card.Img
+            className={styles.Img}
             src={
               movie.backdrop_path
                 ? `https://www.themoviedb.org/t/p/original${movie.backdrop_path}`
@@ -38,48 +48,53 @@ function MovieDetailsPage() {
             alt={movie.title}
           />
           <Card.ImgOverlay>
-            <Card.Title>{movie.original_title}</Card.Title>
-            <Card.Text>{movie.overview}</Card.Text>
+            <Card.Title>
+              {movie.title}(
+              {movie.release_date && movie.release_date.slice(0, 4)})
+            </Card.Title>
             <Card.Text>
+              <p>{`USER SCORE: ${Math.trunc(movie.vote_average * 10)}%`}</p>
+            </Card.Text>
+            <Card.Text>
+              <p>OVERVIEW</p>
+              {movie.overview}
+            </Card.Text>
+            <Card.Text>
+              <p>GENRES</p>
               {movie.genres && movie.genres.map(g => g.name).join(', ')}
             </Card.Text>
           </Card.ImgOverlay>
-          <p>{`User score: ${Math.trunc(movie.vote_average * 10)}%`}</p>
-          {movie.release_date && movie.release_date.slice(0, 4)}
         </Card>
 
-        <Tab.Container id="list-group-tabs-example" defaultActiveKey="#link1">
-          <Row>
-            <Col sm={4}>
-              <ListGroup>
-                <ListGroup.Item action href="#link1">
+        <div className={styles.AdditionalInformationContainer}>
+          <h4>Additional information</h4>
+
+          <ul className={styles.List}>
+            <li className={styles.Item}>
+              <NavLink to={`${url}/cast`} className={styles.Link}>
+                <ListGroup.Item action variant="warning">
                   Cast
                 </ListGroup.Item>
-                <ListGroup.Item action href="#link2">
+              </NavLink>
+            </li>
+
+            <li className={styles.Item}>
+              <NavLink to={`${url}/reviews`} className={styles.Link}>
+                <ListGroup.Item action variant="info">
                   Reviews
                 </ListGroup.Item>
-              </ListGroup>
-            </Col>
-            <Col sm={8}>
-              <Tab.Content>
-                <Tab.Pane eventKey="#link1">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Exercitationem delectus soluta cum quisquam mollitia illo
-                  animi commodi, recusandae nihil. Obcaecati explicabo
-                  perspiciatis nostrum officia molestias expedita cupiditate
-                  dolorem enim! Inventore. Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Earum deleniti omnis quidem
-                  quisquam odio nostrum, assumenda magnam neque minus
-                  voluptatibus natus aliquid, quasi repellendus error repellat
-                  officiis? Repudiandae, earum cupiditate.
-                </Tab.Pane>
-                <Tab.Pane eventKey="#link2">
-                  gggggggggggggggggggggggggggggggggggggggggggggg
-                </Tab.Pane>
-              </Tab.Content>
-            </Col>
-          </Row>
-        </Tab.Container>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <Route path={`${url}/cast`}>
+          <Cast movieId={movieId} />
+        </Route>
+
+        <Route path={`${url}/reviews`}>
+          <Reviews movieId={movieId} />
+        </Route>
       </Container>
     </section>
   );
