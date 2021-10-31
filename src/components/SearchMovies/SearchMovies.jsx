6a69692adefc77f9a41943c92bfa9a1404266b6f
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-10-31 19:02:21 +0100

    Update SearchMovies.jsx

--- src/components/SearchMovies/SearchMovies.jsx
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -1,19 +1,50 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+import PropTypes from 'prop-types';
+import { ImSearch } from 'react-icons/im';
+
 import { InputGroup, Button, FormControl } from 'react-bootstrap';
 
 import styles from './SearchMovies.module.css';
 
-function SearchMovies() {
+function SearchMovies({ onSubmit }) {
+  const location = useLocation();
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    const queryValue = new URLSearchParams(location.search).get('query');
+
+    if (queryValue === null) {
+      return;
+    }
+
+    setValue(queryValue);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleChange = e => {
+    setValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onSubmit(value.trim().toLowerCase());
+  };
   return (
-    <InputGroup className="mb-3">
-      <FormControl
-        placeholder="Search movies ..."
-        aria-label="Search movies"
-        aria-describedby="basic-addon2"
-      />
-      <Button variant="outline-secondary" id="button-addon2">
-        Search
-      </Button>
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search movies ..."
+          aria-label="Search movies"
+          aria-describedby="basic-addon2"
+          onChange={handleChange}
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+          Search
+        </Button>
+      </InputGroup>
+    </form>
   );
 }
 
