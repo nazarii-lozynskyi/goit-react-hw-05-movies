53a4582cfc3b834f943434b51aa8a5f02326e914
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-01 11:33:04 +0100

    Update PageHeading.jsx

--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -1,7 +1,15 @@
+import Bounce from 'react-reveal/Bounce';
+
 import styles from './PageHeading.module.css';
 
 function PageHeading({ text }) {
-  return <h1 className={styles.Heading}>{text}</h1>;
+  return (
+    <h1 className={styles.Heading}>
+      <Bounce right cascade>
+        {text}
+      </Bounce>
+    </h1>
+  );
 }
 
 export default PageHeading;
