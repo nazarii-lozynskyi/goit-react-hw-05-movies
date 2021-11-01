f7bb5aab19264496031abf43161edd38b056e729
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-11-01 12:14:05 +0100

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,36 +3,39 @@ import { NavLink } from 'react-router-dom';
 import Zoom from 'react-reveal/Zoom';
 
 import styles from './Navigation.module.css';
+import { Container } from 'react-bootstrap';
 
 const Navigation = () => (
-  <nav>
-    <ul className={styles.List}>
-      <li className={styles.Item}>
-        <NavLink
-          exact
-          to="/"
-          className={styles.Link}
-          activeClassName={styles.ActiveLink}
-        >
-          <Zoom right cascade>
-            Home
-          </Zoom>
-        </NavLink>
-      </li>
+  <Container>
+    <nav>
+      <ul className={styles.List}>
+        <li className={styles.Item}>
+          <NavLink
+            exact
+            to="/"
+            className={styles.Link}
+            activeClassName={styles.ActiveLink}
+          >
+            <Zoom right cascade>
+              Home
+            </Zoom>
+          </NavLink>
+        </li>
 
-      <li className={styles.Item}>
-        <NavLink
-          to="/movies"
-          className={styles.Link}
-          activeClassName={styles.ActiveLink}
-        >
-          <Zoom right cascade>
-            Movies
-          </Zoom>
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
+        <li className={styles.Item}>
+          <NavLink
+            to="/movies"
+            className={styles.Link}
+            activeClassName={styles.ActiveLink}
+          >
+            <Zoom right cascade>
+              Movies
+            </Zoom>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  </Container>
 );
 
 export default Navigation;
